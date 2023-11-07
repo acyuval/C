f9a5280f045dbf49f7f2738bc513333780c8be64
yuval.glikman <yuval@glikman.com>
2023-11-07 12:19:07 +0200

    wc2 fixed

--- c/ws2/wc2_test.c
+++ c/ws2/wc2_test.c
@@ -3,6 +3,65 @@
 #include "wc2.h"
 
 
+int TestSwapInt()
+{
+	int one = 4; 
+	int two = 12; 
+	printf("\nbefore:%d::::%d\n" , one , two);
+	SwapInt(&one, &two);
+	printf("\nafter:%d::::%d\n" , one , two);
+	return 0;
+}
+
+int TestSwapSizeT()
+{
+	size_t one = 4; 
+	size_t two = 12; 
+	
+	size_t *one_ptr = &one; 
+	size_t *two_ptr = &two; 
+	
+	printf("\nbefore:%lu::::%lu\n" , one , two);
+	SwapSizeT(&one, &two);
+	printf("\nafter:%lu::::%lu\n" , one , two);
+	printf("\nbefore:%lu::::%lu\n" ,*one_ptr, *two_ptr);
+	SwapSizeT(one_ptr, two_ptr);
+	printf("\nafter:%lu::::%lu\n" ,*one_ptr, *two_ptr);
+	
+	return 0;
+}
+int TestSwapSizeTPtr()
+{
+	size_t one = 4; 
+	size_t two = 12; 
+	
+	size_t *one_ptr = &one; 
+	size_t *two_ptr = &two; 
+	
+	printf("\nbefore:%lu::::%lu\n" ,*one_ptr, *two_ptr);
+	SwapSizeT(one_ptr, two_ptr);
+	printf("\nafter:%lu::::%lu\n" ,*one_ptr, *two_ptr);
+	
+	return 0;
+}
+
+int TestCopyIntArr()
+{
+
+	int arr[] = {5 , 6 ,7 ,9 ,10};
+	int size_arr = sizeof(arr)/sizeof(int);
+	int *cp_arr = CopyIntArr(5 , arr);	
+	int i = 0;
+	printf("array copy is: ");
+	for(i=0;i<size_arr;i++)
+	{
+		
+		printf("%d, " , *(cp_arr+i));
+	}
+	free(cp_arr);
+	return (0);
+}
+
 int main()
 {
 	printf("\nswap int : \n");
