e5cba9810b8319c38ab0c2a338976e7f0f15e50e
yuval.glikman <yuval@glikman.com>
2024-01-05 21:01:15 +0200

    with power and brackets

--- projects/calculator/calculator.c
+++ projects/calculator/calculator.c
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <math.h>
 
 #include "../calculator/stack.h"
 #include "../calculator/calculator.h"
@@ -29,11 +30,12 @@ typedef enum States
 typedef enum Events
 {
     E_DIGITS = 0,
+    E_OPEN_BRACKER,
     E_PLUS,
     E_MINUS, 
     E_MULTI,
     E_DIV, 
-    E_OPEN_BRACKER,
+    E_POWER,
     E_CLOSE_BRACKET,
     E_TERMINATE,
     E_ANY_OTHER,
@@ -61,18 +63,21 @@ static void InitOP2Func(computation_funcs * OP2Func);
 static void InitOperationVSEvent2Func(handlers operation_vs_event[][NUM_OF_EVENTS]);
 static state_t NumHandler(params * params_struct);
 static state_t OPHandler(params * params_struct);
+static state_t OpenBracketHandler(params * params_struct);
+static state_t CloseBracketHandler(params * params_struct);
+
 static state_t END(params * params_struct);
 static state_t Exit(params * params_struct);
-static state_t OpenBracketHandler(params * params_struct);
 static state_t Invalid(params * params_struct);
-static state_t CloseBracketHandler(params * params_struct);
 static state_t Show(params * params_struct);
 static state_t Exit(params * params_struct);
 
+static int Power(params * params_struct);
 static int Substract(params * params_struct);
 static int Add(params * params_struct);
 static int Divide(params * params_struct);
 static int Multi(params * params_struct);
+static int OpenBracketDummy(params * params_struct);
 
 static void ComputeTwoNums(params * params_struct);
 static void PopTwoNums(params * params_struct, double nums[]);
@@ -118,7 +123,6 @@ status_t Calculate(const char * expression , double * result)
 ******************************************************************************/
 static void InitChangeToEnums(events_t * CHANGE2ENUM)
 {
-
     int i = 0; 
     events_t LUT[256] = {E_ANY_OTHER};
 
@@ -133,11 +137,13 @@ static void InitChangeToEnums(events_t * CHANGE2ENUM)
     LUT['/'] =  E_DIV;
     LUT['('] =  E_OPEN_BRACKER;
     LUT[')'] =  E_CLOSE_BRACKET;
+    LUT['^'] =  E_POWER;
     LUT['\0'] =  E_TERMINATE;
     
     memcpy(CHANGE2ENUM, &LUT, sizeof(LUT));
     return;
 }
+
 static void InitStructParmms(params * params, stack_t * ops_stack, stack_t * nums_stack ,char * expression, events_t char2ENUM[256], computation_funcs op2func[NUM_OF_EVENTS], double * result)
 {
     params->ops_stack = ops_stack;
@@ -151,8 +157,7 @@ static void InitStructParmms(params * params, stack_t * ops_stack, stack_t * num
 
 static void InitOP2Func(computation_funcs * op2func)
 {
-    
-   computation_funcs LUT[NUM_OF_EVENTS] = {NULL,Add, Substract, Multi, Divide , NULL , NULL , NULL,NULL};
+   computation_funcs LUT[NUM_OF_EVENTS] = {NULL,OpenBracketDummy,Add, Substract, Multi, Divide,Power,NULL,NULL,NULL};
    
     memcpy(op2func,&LUT, sizeof(LUT));
     return;
@@ -160,9 +165,9 @@ static void InitOP2Func(computation_funcs * op2func)
 
 static void InitOperationVSEvent2Func(handlers operation_vs_event[][NUM_OF_EVENTS])
 {
-    handlers LUT[NUM_OF_STATES][NUM_OF_EVENTS] = {{NumHandler, Invalid,Invalid,Invalid,Invalid,OpenBracketHandler, Invalid,Invalid,Invalid},
-                                                    {Invalid, OPHandler,OPHandler,OPHandler,OPHandler,Invalid, Invalid,Show,Invalid},
-                                                    {Exit, Exit,Exit,Exit,Exit,Exit,Exit,Exit,Exit}};
+    handlers LUT[NUM_OF_STATES][NUM_OF_EVENTS] = {{NumHandler,OpenBracketHandler,Invalid,Invalid,Invalid,Invalid,Invalid,Invalid,Invalid,Invalid},
+                                                  {Invalid,Invalid,OPHandler,OPHandler,OPHandler,OPHandler,OPHandler,CloseBracketHandler,Show,Invalid},
+                                                    {Exit, Exit,Exit,Exit,Exit,Exit,Exit,Exit,Exit,Exit}};
                                                     
    
     memcpy(operation_vs_event,LUT, sizeof(LUT));
@@ -209,10 +214,39 @@ static state_t OPHandler(params * params_struct)
 }
 
 
+static state_t OpenBracketHandler(params * params_struct)
+{
+    events_t operator  = params_struct->char2ENUM[*params_struct->expression];
+    StackPush(params_struct->ops_stack, &operator);
+    ++(params_struct->expression);
+    return S_WAITING4NUM;
+}
+
+static state_t CloseBracketHandler(params * params_struct)
+{
+    events_t operator  = params_struct->char2ENUM[*params_struct->expression];
+    events_t prev_opreator = *(events_t *)StackPeek(params_struct->ops_stack);
+    while ((FALSE == StackIsEmpty(params_struct->ops_stack)) && prev_opreator != E_OPEN_BRACKER)
+    {
+        ComputeTwoNums(params_struct);
+        prev_opreator = *(events_t *)StackPeek(params_struct->ops_stack);
+    }
+    StackPop(params_struct->ops_stack);
+    ++(params_struct->expression);
+    return S_WAITING4OP;
+}
+
 static state_t END(params * params_struct)
 {
     (void)params_struct;
     return S_INVALID;
+}
+
+
+static int OpenBracketDummy(params * params_struct)
+{
+    (void)params_struct;
+    return 0;
 }
 
 
@@ -223,7 +257,6 @@ static void PopTwoNums(params * params_struct, double nums[])
     nums[2] = *(double *)StackPeek(params_struct->nums_stack);
     StackPop(params_struct->nums_stack); 
 }
-
 
 static int Add(params * params_struct)
 {
@@ -237,6 +270,17 @@ static int Add(params * params_struct)
     return 0;
 }
 
+static int Power(params * params_struct)
+{
+    double nums[2] = {0};
+    double res = 0 ; 
+
+    PopTwoNums(params_struct, nums);
+    res = pow(nums[2],nums[1]);
+    StackPush(params_struct->nums_stack, &res);
+    StackPop(params_struct->ops_stack);
+    return 0;
+}
 
 static int Substract(params * params_struct)
 {
@@ -282,23 +326,6 @@ static state_t Exit(params * params_struct)
 }
 
 
-static state_t OpenBracketHandler(params * params_struct)
-{
-    (void)params_struct;
-    return S_INVALID;
-}
-
-static state_t CloseBracketHandler(params * params_struct)
-{
-    (void)params_struct;
-    return S_INVALID;
-}
-
-static state_t PushCBracket(params * params_struct)
-{
-    (void)params_struct;
-    return S_INVALID;
-}
 
 static state_t Invalid(params * params_struct)
 {
@@ -313,7 +340,7 @@ static state_t Show(params * params_struct)
     {
         ComputeTwoNums(params_struct);
     }
-    
+
     params_struct->result = (double *)StackPeek(params_struct->nums_stack);
 
     printf("result: %f", *params_struct->result); 
--- projects/calculator/calculator_test.c
+++ projects/calculator/calculator_test.c
@@ -6,7 +6,7 @@
 int main()
 {
     double result = 0;
-    const char * str = "1+6*2+3-4";
+    const char * str = "5*3*(2+3)^2";
     Calculate(str, &result);
     return (0);
 }
